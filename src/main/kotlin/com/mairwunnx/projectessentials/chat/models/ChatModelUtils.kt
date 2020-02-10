e153e18d85bdb1fda5a6f498ab77ae8a308ff11a
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-02-10 20:36:02 +0300

    `warpsConfig` renamed to `chatConfig` in ChatModelUtils.kt.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/chat/models/ChatModelUtils.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/models/ChatModelUtils.kt
@@ -6,21 +6,21 @@ import org.apache.logging.log4j.LogManager
 import java.io.File
 
 object ChatModelUtils {
-    private val warpsConfig = MOD_CONFIG_FOLDER + File.separator + "chat.json"
+    private val chatConfig = MOD_CONFIG_FOLDER + File.separator + "chat.json"
     private val logger = LogManager.getLogger()
     var chatModel = ChatModel()
 
     fun loadData() {
         logger.info("Loading chat configuration")
-        if (!File(warpsConfig).exists()) {
+        if (!File(chatConfig).exists()) {
             logger.warn("Chat configuration file not exist! creating it now!")
             File(MOD_CONFIG_FOLDER).mkdirs()
             val defaultConfig = jsonInstance.stringify(
                 ChatModel.serializer(), chatModel
             )
-            File(warpsConfig).writeText(defaultConfig)
+            File(chatConfig).writeText(defaultConfig)
         }
-        val warpsConfigRaw = File(warpsConfig).readText()
+        val warpsConfigRaw = File(chatConfig).readText()
         chatModel = jsonInstance.parse(ChatModel.serializer(), warpsConfigRaw)
     }
 
@@ -29,6 +29,6 @@ object ChatModelUtils {
         val spawnConfig = jsonInstance.stringify(
             ChatModel.serializer(), chatModel
         )
-        File(warpsConfig).writeText(spawnConfig)
+        File(chatConfig).writeText(spawnConfig)
     }
 }
