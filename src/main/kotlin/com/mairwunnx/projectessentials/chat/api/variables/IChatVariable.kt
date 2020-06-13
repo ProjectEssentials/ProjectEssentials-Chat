42ccb49f38cbbf71e3c165938f7b2dd492275a8a
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-13 17:06:48 +0300

    IChatVariable.kt interface added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/api/variables/IChatVariable.kt
@@ -0,0 +1,23 @@
+package com.mairwunnx.projectessentials.chat.api.variables
+
+import net.minecraft.entity.player.ServerPlayerEntity
+
+/**
+ * Interface for chat variable, (basically for replacing
+ * chat variables which starts with `%` symbol.)
+ * @since 2.0.0.
+ */
+interface IChatVariable {
+    /**
+     * Chat variable literal, without `%` symbol.
+     * @since 2.0.0.
+     */
+    val variable: String
+
+    /**
+     * @param player player to process this.
+     * @return variable result.
+     * @since 2.0.0.
+     */
+    fun process(player: ServerPlayerEntity): String
+}
