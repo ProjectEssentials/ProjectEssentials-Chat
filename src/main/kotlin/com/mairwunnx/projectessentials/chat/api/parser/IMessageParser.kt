dad70f6750584331c6699babd2524176cfc709d4
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-13 17:48:47 +0300

    IMessageParser.kt interface added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/api/parser/IMessageParser.kt
@@ -0,0 +1,17 @@
+package com.mairwunnx.projectessentials.chat.api.parser
+
+import net.minecraft.entity.player.ServerPlayerEntity
+import net.minecraft.util.text.ITextComponent
+
+/**
+ * Base interface for message parsers.
+ * @since 2.0.0.
+ */
+interface IMessageParser {
+    /**
+     * Parses the message and changes message.
+     * @return ready to sent message as string.
+     * @since 2.0.0.
+     */
+    fun parse(sender: ServerPlayerEntity, message: ITextComponent): String
+}
