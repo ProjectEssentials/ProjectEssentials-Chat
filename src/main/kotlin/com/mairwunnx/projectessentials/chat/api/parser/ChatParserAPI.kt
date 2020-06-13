2db42fa69af577f25a5f2d216af645485efa9393
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-13 17:53:39 +0300

    ChatParserAPI.kt implemented.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/api/parser/ChatParserAPI.kt
@@ -0,0 +1,18 @@
+package com.mairwunnx.projectessentials.chat.api.parser
+
+import com.mairwunnx.projectessentials.chat.impl.DefaultMessageParser
+
+/**
+ * Class for interacting with active chat parser.
+ * @since 2.0.0.
+ */
+object ChatParserAPI {
+    /**
+     * Active chat parser, you can change that at any time.
+     *
+     * By default that have value [DefaultMessageParser].
+     *
+     * @since 2.0.0.
+     */
+    var parser: IMessageParser = DefaultMessageParser()
+}
