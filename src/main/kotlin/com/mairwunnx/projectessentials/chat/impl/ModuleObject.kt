0964fddec3270d84a68493eba2daf488763fe66d
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-22 23:48:02 +0300

    onChatMessage handling added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/ModuleObject.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/ModuleObject.kt
@@ -2,15 +2,16 @@
 
 package com.mairwunnx.projectessentials.chat.impl
 
-import com.mairwunnx.projectessentials.chat.commands.*
-import com.mairwunnx.projectessentials.chat.impl.configurations.ChatSettingsConfiguration
-import com.mairwunnx.projectessentials.chat.impl.configurations.MutedPlayersConfiguration
+import com.mairwunnx.projectessentials.chat.impl.handlers.ChatMessageHandler
 import com.mairwunnx.projectessentials.chat.impl.handlers.ReceiveMessageHandler
+import com.mairwunnx.projectessentials.chat.localizations
+import com.mairwunnx.projectessentials.chat.providers
 import com.mairwunnx.projectessentials.core.api.v1.localization.LocalizationAPI
 import com.mairwunnx.projectessentials.core.api.v1.module.IModule
 import com.mairwunnx.projectessentials.core.api.v1.providers.ProviderAPI
 import net.minecraftforge.client.event.ClientChatReceivedEvent
 import net.minecraftforge.common.MinecraftForge.EVENT_BUS
+import net.minecraftforge.event.ServerChatEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import org.apache.logging.log4j.LogManager
@@ -24,40 +25,15 @@ internal class ModuleObject : IModule {
 
     private val logger = LogManager.getLogger()
 
-    private val providers = listOf(
-        ChatSettingsConfiguration::class.java,
-        MutedPlayersConfiguration::class.java,
-        ModuleObject::class.java,
-        ClearChatCommand::class.java,
-        MuteCommand::class.java,
-        UnmuteCommand::class.java,
-        UnmuteAllCommand::class.java,
-        MutedPlayersCommand::class.java
-        // PrefixCommand::class.java,
-        // SuffixCommand::class.java
-    )
-
     init {
         EVENT_BUS.register(this)
         providers.forEach(ProviderAPI::addProvider)
-        initLocalization()
-    }
-
-    private fun initLocalization() {
-        LocalizationAPI.apply(this.javaClass) {
-            mutableListOf(
-                "/assets/projectessentialschat/lang/pt_br.json",
-                "/assets/projectessentialschat/lang/zh_cn.json",
-                "/assets/projectessentialschat/lang/de_de.json",
-                "/assets/projectessentialschat/lang/ru_ru.json",
-                "/assets/projectessentialschat/lang/en_us.json",
-                "/assets/projectessentialschat/lang/es_cl.json",
-                "/assets/projectessentialschat/lang/fr_fr.json",
-                "/assets/projectessentialschat/lang/sv_se.json"
-            )
-        }
+        LocalizationAPI.apply(this.javaClass) { localizations }
     }
 
     @SubscribeEvent
     fun onReceivedMessage(event: ClientChatReceivedEvent) = ReceiveMessageHandler.handle(event)
+
+    @SubscribeEvent
+    fun onChatMessage(event: ServerChatEvent) = ChatMessageHandler.handle(event)
 }
