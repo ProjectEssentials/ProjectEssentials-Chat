82d5bdc2271615673e20637109e5387b9844bdf1
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-22 23:47:47 +0300

    Providers.kt added with localizations and class references.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/Providers.kt
@@ -0,0 +1,30 @@
+package com.mairwunnx.projectessentials.chat
+
+import com.mairwunnx.projectessentials.chat.commands.*
+import com.mairwunnx.projectessentials.chat.impl.ModuleObject
+import com.mairwunnx.projectessentials.chat.impl.configurations.ChatSettingsConfiguration
+import com.mairwunnx.projectessentials.chat.impl.configurations.MutedPlayersConfiguration
+
+internal val providers = listOf(
+    ChatSettingsConfiguration::class.java,
+    MutedPlayersConfiguration::class.java,
+    ModuleObject::class.java,
+    ClearChatCommand::class.java,
+    MuteCommand::class.java,
+    UnmuteCommand::class.java,
+    UnmuteAllCommand::class.java,
+    MutedPlayersCommand::class.java
+    // PrefixCommand::class.java,
+    // SuffixCommand::class.java
+)
+
+internal val localizations = mutableListOf(
+    "/assets/projectessentialschat/lang/pt_br.json",
+    "/assets/projectessentialschat/lang/zh_cn.json",
+    "/assets/projectessentialschat/lang/de_de.json",
+    "/assets/projectessentialschat/lang/ru_ru.json",
+    "/assets/projectessentialschat/lang/en_us.json",
+    "/assets/projectessentialschat/lang/es_cl.json",
+    "/assets/projectessentialschat/lang/fr_fr.json",
+    "/assets/projectessentialschat/lang/sv_se.json"
+)
