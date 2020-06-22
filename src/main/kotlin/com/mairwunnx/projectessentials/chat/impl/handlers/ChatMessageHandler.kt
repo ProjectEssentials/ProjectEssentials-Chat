dd65cd1e2094640c5f2c6ee40905db0e34c5ada0
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-22 23:48:17 +0300

    ChatMessageHandler.kt added w/o implementation.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/handlers/ChatMessageHandler.kt
@@ -0,0 +1,9 @@
+package com.mairwunnx.projectessentials.chat.impl.handlers
+
+import net.minecraftforge.event.ServerChatEvent
+
+internal object ChatMessageHandler {
+    fun handle(event: ServerChatEvent) {
+
+    }
+}
