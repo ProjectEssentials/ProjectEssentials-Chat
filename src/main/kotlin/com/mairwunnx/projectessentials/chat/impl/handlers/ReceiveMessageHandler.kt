d4a7827c501a7bc0bb39ee5e74609a3554e7080b
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-22 23:41:57 +0300

    ReceiveMessageHandler.kt implemented.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/handlers/ReceiveMessageHandler.kt
@@ -0,0 +1,18 @@
+package com.mairwunnx.projectessentials.chat.impl.handlers
+
+import com.mairwunnx.projectessentials.chat.chatSettingsConfiguration
+import net.minecraftforge.client.event.ClientChatReceivedEvent
+
+object ReceiveMessageHandler {
+    fun handle(event: ClientChatReceivedEvent) {
+        fun cancelIf(key: String) {
+            if ("key='$key" in event.message.toString()) {
+                { event.isCanceled = true }.let { return }
+            }
+        }
+        if (!chatSettingsConfiguration.events.joinMessageEnabled) cancelIf("multiplayer.player.joined'")
+        if (!chatSettingsConfiguration.events.leftMessageEnabled) cancelIf("multiplayer.player.left'")
+        if (!chatSettingsConfiguration.events.advancementsEnabled) cancelIf("chat.type.advancement'")
+        if (!chatSettingsConfiguration.events.deathMessagesEnabled) cancelIf("death.")
+    }
+}
