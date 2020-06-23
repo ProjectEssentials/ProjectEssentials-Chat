058fe5121dbb38f90064688b6717bebf11388b45
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-23 22:31:49 +0300

    WorldVariable.kt improved.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/variables/WorldVariable.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/variables/WorldVariable.kt
@@ -4,10 +4,17 @@ import com.mairwunnx.projectessentials.chat.api.variables.IChatVariable
 import com.mairwunnx.projectessentials.core.api.v1.extensions.capitalizeWords
 import com.mairwunnx.projectessentials.core.api.v1.extensions.currentDimensionName
 import net.minecraft.entity.player.ServerPlayerEntity
+import net.minecraft.util.text.ITextComponent
+import net.minecraft.util.text.TextComponentUtils
 
 object WorldVariable : IChatVariable {
     override val variable = "world"
     override fun process(
         player: ServerPlayerEntity
-    ) = player.currentDimensionName.replace("_", " ").capitalizeWords()
+    ): ITextComponent = TextComponentUtils.toTextComponent {
+        player.currentDimensionName
+            .dropWhile { it == ':' }.drop(1)
+            .replace("_", " ")
+            .capitalizeWords()
+    }
 }
