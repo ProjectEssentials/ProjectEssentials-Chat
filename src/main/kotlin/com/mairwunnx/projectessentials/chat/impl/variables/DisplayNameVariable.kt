1bf021e532fc56771479377b0f3bea28f11afc8e
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-23 22:33:01 +0300

    DisplayNameVariable.kt improved.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/variables/DisplayNameVariable.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/variables/DisplayNameVariable.kt
@@ -2,8 +2,17 @@ package com.mairwunnx.projectessentials.chat.impl.variables
 
 import com.mairwunnx.projectessentials.chat.api.variables.IChatVariable
 import net.minecraft.entity.player.ServerPlayerEntity
+import net.minecraft.util.text.ITextComponent
+import net.minecraft.util.text.Style
+import net.minecraft.util.text.event.ClickEvent
 
 object DisplayNameVariable : IChatVariable {
     override val variable = "display_name"
-    override fun process(player: ServerPlayerEntity): String = player.displayName.formattedText
+    override fun process(player: ServerPlayerEntity): ITextComponent = player.displayName.setStyle(
+        Style().apply {
+            clickEvent = ClickEvent(
+                ClickEvent.Action.SUGGEST_COMMAND, "/m ${player.gameProfile.name!!}"
+            )
+        }
+    )
 }
