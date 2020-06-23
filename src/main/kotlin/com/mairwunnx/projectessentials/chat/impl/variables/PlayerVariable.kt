6040b2dbefaf330c4612afe3fb3a8479992be36d
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-23 22:32:18 +0300

    PlayerVariable.kt improved.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/variables/PlayerVariable.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/variables/PlayerVariable.kt
@@ -2,8 +2,20 @@ package com.mairwunnx.projectessentials.chat.impl.variables
 
 import com.mairwunnx.projectessentials.chat.api.variables.IChatVariable
 import net.minecraft.entity.player.ServerPlayerEntity
+import net.minecraft.util.text.ITextComponent
+import net.minecraft.util.text.Style
+import net.minecraft.util.text.TextComponentUtils
+import net.minecraft.util.text.event.ClickEvent
 
 object PlayerVariable : IChatVariable {
     override val variable = "player"
-    override fun process(player: ServerPlayerEntity) = player.gameProfile.name!!
+    override fun process(
+        player: ServerPlayerEntity
+    ): ITextComponent = TextComponentUtils.toTextComponent { player.gameProfile.name!! }.setStyle(
+        Style().apply {
+            clickEvent = ClickEvent(
+                ClickEvent.Action.SUGGEST_COMMAND, "/m ${player.gameProfile.name!!}"
+            )
+        }
+    )
 }
