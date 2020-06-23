c046fcc0812092d4a9d6c52fd04f292106d682c0
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-23 23:20:30 +0300

    Variables added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/Variables.kt
@@ -0,0 +1,12 @@
+package com.mairwunnx.projectessentials.chat.impl
+
+import com.mairwunnx.projectessentials.chat.impl.variables.*
+
+val variables = listOf(
+    DisplayNameVariable,
+    LvLVariable,
+    PlayerVariable,
+    TeamVariable,
+    TimeVariable,
+    WorldVariable
+)
