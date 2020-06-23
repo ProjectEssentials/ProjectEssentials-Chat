28a4c38a04018b3e35114b8fa10ff02386eea823
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-23 22:32:00 +0300

    TimeVariable.kt improved.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/variables/TimeVariable.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/variables/TimeVariable.kt
@@ -2,11 +2,24 @@ package com.mairwunnx.projectessentials.chat.impl.variables
 
 import com.mairwunnx.projectessentials.chat.api.variables.IChatVariable
 import net.minecraft.entity.player.ServerPlayerEntity
+import net.minecraft.util.text.ITextComponent
+import net.minecraft.util.text.Style
+import net.minecraft.util.text.TextComponentUtils
+import net.minecraft.util.text.event.HoverEvent
 import java.time.ZonedDateTime.now
 
 object TimeVariable : IChatVariable {
     override val variable = "time"
     override fun process(
         player: ServerPlayerEntity
-    ) = now().toLocalTime().toString().replace(Regex("\\..*"), "")
+    ): ITextComponent = TextComponentUtils.toTextComponent {
+        now().toLocalTime().toString().replace(Regex("\\..*"), "")
+    }.setStyle(
+        Style().apply {
+            hoverEvent = HoverEvent(
+                HoverEvent.Action.SHOW_TEXT,
+                TextComponentUtils.toTextComponent { "Message sent at ${now().toLocalTime()}" }
+            )
+        }
+    )
 }
