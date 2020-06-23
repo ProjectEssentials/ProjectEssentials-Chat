6f635613ffede06c7740f635842a8f3546a6d442
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-23 22:32:26 +0300

    LvLVariable.kt improved.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/variables/LvLVariable.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/chat/impl/variables/LvLVariable.kt
@@ -2,8 +2,12 @@ package com.mairwunnx.projectessentials.chat.impl.variables
 
 import com.mairwunnx.projectessentials.chat.api.variables.IChatVariable
 import net.minecraft.entity.player.ServerPlayerEntity
+import net.minecraft.util.text.ITextComponent
+import net.minecraft.util.text.TextComponentUtils
 
 object LvLVariable : IChatVariable {
     override val variable = "lvl"
-    override fun process(player: ServerPlayerEntity) = player.experienceLevel.toString()
+    override fun process(
+        player: ServerPlayerEntity
+    ): ITextComponent = TextComponentUtils.toTextComponent { player.experienceLevel.toString() }
 }
